1548b334e64f93a0e9eb13b3db329c6b330bf16d
Mayank Badola <badola21295@gmail.com>
2016-10-01 08:15:21 +0530

    Unit-tests addition initiated

--- tests/index_test.js
+++ tests/index_test.js
@@ -11,10 +11,22 @@
 const tape = require('tape'),
   validator = require('../index');
 
-tape('function is exported', (test) => {
+tape('Exporting function', (test) => {
   test.plan(2);
 
   test.equal(typeof validator, 'function');
   test.equal(validator.length, 1);
 });
 
+tape('Validating methods', (test) => {
+  test.plan(6);
+
+  const validatorInstance = new validator()
+
+  test.equal(typeof validatorInstance.errored, 'function');
+  test.equal(typeof validatorInstance.validateStructure, 'function');
+  test.equal(typeof validatorInstance.loadFile, 'function');
+  test.equal(typeof validatorInstance.checkFile, 'function');
+  test.equal(typeof validatorInstance.validate, 'function');
+  test.equal(typeof validatorInstance.report, 'function');
+});
