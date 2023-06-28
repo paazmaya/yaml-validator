c3fae0cec331e857c0354915a4fa476fbd916205
Juga Paazmaya <paazmaya@yahoo.com>
2023-06-28 19:58:22 +0300

    Propely generate type dictionaries, while require active LTS of Node.js

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@ It follows the guidance from https://keepachangelog.com/en/1.0.0/.
 
 ## Unreleased
 
+- Minimum supported Node.js version lifted from `14.15.0` to `18.12.0`
+- Typescript type dictionaries were not generated properly
+
 ## `v4.0.1` (2023-06-05)
 
 - It's all ES Modules now
---
+++ types/index.d.ts
@@ -0,0 +1,53 @@
+export default YamlValidatore;
+declare class YamlValidatore {
+    constructor(options: any);
+    options: any;
+    logs: any[];
+    nonValidPaths: any[];
+    inValidFilesCount: number;
+    /**
+     * Store log messages
+     * possible later use by writing a log file.
+     * @param {string} msg Error message
+     * @returns {void}
+     */
+    errored(msg: string): void;
+    /**
+     * Check that the given structure is available.
+     * @param {Object} doc Object loaded from Yaml file
+     * @param {Object} structure Structure requirements
+     * @param {string} parent Address in a dot notation
+     * @returns {Array} List of not found structure paths
+     */
+    validateStructure(doc: Object, structure: Object, parent: string): any[];
+    /**
+     * Parse the given Yaml data.
+     * @param {string} filepath Yaml file path
+     * @param {string} data Yaml data
+     * @returns {string|null} Parsed Yaml or null on failure
+     */
+    loadData(filepath: string, data: string): string | null;
+    /**
+     * Read and parse the given Yaml file.
+     * @param {string} filepath Yaml file path
+     * @returns {string|null} Parsed Yaml or null on failure
+     */
+    loadFile(filepath: string): string | null;
+    /**
+     * Read the given Yaml file, load and check its structure.
+     * @param {string} filepath Yaml file path
+     * @returns {number} 0 when no errors, 1 when errors.
+     */
+    checkFile(filepath: string): number;
+    /**
+     * Create a report out of this, but in reality also run.
+     * @param {array} files List of files that have been checked that they exist
+     * @returns {void}
+     */
+    validate(files: array): void;
+    /**
+     * Create a report out of this, but in reality also run.
+     * @returns {number} 0 when no errors, the count of invalid files otherwise.
+     */
+    report(): number;
+}
